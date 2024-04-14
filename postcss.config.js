6e64261cc57d052a680f5bb8535fba20910515c2
RomanKavandin <kin.tama@yandex.ru>
2024-04-14 03:28:54 +0300

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
 import cssnanoPlugin from "cssnano";
-import postcssExtend from "postcss-extend";
 import postcssPresetEnv from "postcss-preset-env";
 export default {
   plugins: [
     cssnanoPlugin({
       preset: "default",
     }),
-    postcssExtend(),
     postcssPresetEnv({ stage: 1 }),
   ],
 };
